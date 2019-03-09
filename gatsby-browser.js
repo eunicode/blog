6c68a49d5a657e540666670a88d42e801d361998
Eunice Park <28845173+eunicode@users.noreply.github.com>
2019-03-08 23:12:37 -0800

    Global styles w/ CSS file + gatsby-browser.js (no layout component)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,11 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import "typeface-montserrat";
+import "typeface-merriweather";
+
+/* ========================================================================== */
+
+/* Gatsby Tutorial 2: Introduction to Styling in Gatsby
+https://www.gatsbyjs.org/tutorial/part-two/ */
+
+// GLOBAL STYLES + NO SHARED LAYOUT COMPONENT
+import "./src/styles/global.css";
